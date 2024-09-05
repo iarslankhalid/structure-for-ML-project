c3c3bb5101720afbe81edf1430dd038d4780c7e8
iarslankhalid <rj.arslan750@gmail.com>
2024-09-05 06:00:36 +0500

    project structure

---
+++ structure-for-ML-project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='General structure for machine learning projects',
+    author='Arslan Khalid',
+    license='MIT',
+)
